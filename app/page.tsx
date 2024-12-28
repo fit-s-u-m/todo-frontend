3bbe9ab452ae2cd29025bff3b53bc61b63799698
fit-s-u-m <nehemiahmaths@gmail.com>
2024-12-29 02:39:00 +0300

    added a search feature from the server

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import axios from 'axios';
 
 import TodoComponet from '@/components/TodoComponet'
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import { Modal } from '@/components/ModalComponet';
 import CreateTodoComponet from '@/components/CreateTodoComponet';
 
@@ -15,26 +16,46 @@ const fetchTodo = async (): Promise<ITodo[]> => {
   const { data } = await axios.get('http://localhost:8080/todos/');
   return data
 }
+const fetchFilterdTodo = async (search: string): Promise<ITodo[]> => {
+  const { data } = await axios.get(`http://localhost:8080/todos/search/${search}`);
+  return data
+}
 
 
 const Todo = () => {
-  const [todos, setTodos] = useState<ITodo[]>()
+  const [todos, setTodos] = useState<ITodo[]>([])
   const [changed, setChanged] = useState<boolean>()
+  const [search, setSearch] = useState<string>("")
 
   useEffect(() => {
-    fetchTodo().then((todos: ITodo[]) => {
-      setTodos(todos);
-    })
-  }, [changed])
+    if (search == "") {
+      fetchTodo().then((todos: ITodo[]) => {
+        setTodos(todos);
+      })
+    }
+    else {
+      fetchFilterdTodo(search).then((todos: ITodo[]) => {
+        setTodos(todos);
+      })
+    }
+  }, [changed, search])
+
 
   return (
     <div className='w-[50%] m-auto'>
       <h1 className='text-4xl font-bold text-center mb-20 mt-5'>
         Todo List
       </h1>
+      <Input
+        type="text"
+        placeholder="Search todos..."
+        value={search}
+        onChange={(e) => { setSearch(e.target.value) }}
+        className="w-full mb-5"
+      />
       <ul className="flex flex-col gap-5 justfiy-center items-start mt-8 ">
         {
-          (todos == undefined || todos.length == 0) ?
+          (todos.length == 0) ?
             <p className="text-red-500 font-bold ">no todos</p> :
             (todos.map((todo: ITodo) => (
               <li key={todo.id} className='block w-[100%]'>
